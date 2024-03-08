17383853d700eebb4c001654d6f797ab395e36cc
CaioEmPessoa <caiopolonioabruno@gmail.com>
2024-03-08 17:48:31 -0300

    added buttons equivalent to folders in the root of the app

--- run.py
+++ run.py
@@ -19,6 +19,19 @@ class MainWindow(tk.Tk):
         self.geometry("500x500")
         self.title("File Organizer")
 
+    def search_folders(self):
+        #self.root_folders = ["pasta1", "pasta2", "pasta3", "pasta4", "pasta5", "pasta6", "pasta 7"]
+        self.root_folders = next(os.walk('.'))[1]
+        index = 0
+
+        for y in range(9):  # Number of rows
+            for x in range(3):  # Number of columns
+                if index < len(self.root_folders):
+                    self.folder_btn = tk.Button(master=self, text=self.root_folders[index], command=lambda s=self.root_folders[index]: print(s))
+                    self.folder_btn.grid(row=y+3, column=x)
+                    index+=1
+
+
     def search_media(self):
         self.root_files = os.listdir(self.APP_PATH)
         self.root_media = []
@@ -102,7 +115,7 @@ class MainWindow(tk.Tk):
         self.image_label = tk.Label(master=self)
         self.player = TkinterVideo(master=self, scaled=True)
 
-        self.previous_btn = tk.Button(master=self, text="<- previous", command=lambda: self.previous_media(),width=10)
+        self.previous_btn = tk.Button(master=self, text="<- previous", command=lambda: self.previous_media(), width=10)
         self.previous_btn.grid(row=1, column=0, padx=10, pady=10)
 
         self.play_button = tk.Button(master=self, text="play", command=lambda: self.video_start(), width=10)
@@ -116,4 +129,5 @@ main.buttons()
 main.search_media()
 main.update()
 main.show_media(main.root_media[0])
+main.search_folders()
 main.mainloop()
